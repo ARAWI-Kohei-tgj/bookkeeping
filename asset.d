f49c4e9f3959d965fd2b9c4f361acbb1326a988e
ARAI Kohei <56864935+ARAWI-Kohei-tgj@users.noreply.github.com>
2022-01-17 22:40:22 +0900

    Add files via upload

---
+++ asset.d
@@ -0,0 +1,168 @@
+module bookkeeping.asset;
+
+/*************************************************************
+ * 有形資産の減価償却
+ *************************************************************/
+struct FixedAsset{
+  import std.datetime: Date, Month;
+
+  string assetName;
+
+  this(in string name, in Date acqDate, in string titleStr,
+       in uint price, in uint period) @safe pure nothrow @nogc{
+    assetName= name;
+    _acquisitionDate= acqDate;
+    _accountTitle= titleStr;
+    _priceInit= price;
+    _economicLife= period;
+  }
+
+  @property @safe pure nothrow @nogc const{
+    /*****************************************
+     * 会計科目
+     *****************************************/
+    string accountTitle(){
+      return _accountTitle;
+    }
+
+    /*****************************************
+     * 年間の償却金額
+     *
+     * 小数点以下は四捨五入
+     *****************************************/
+    uint priceDeprecBasic(){
+      import std.math: round;
+      return cast(uint)(round(cast(double)(_priceInit)/_economicLife));
+    }
+
+    /*****************************************
+     * 償却期間
+     *****************************************/
+    uint economicLife(){
+      return _economicLife;
+    }
+  }
+
+  @safe pure const{
+    /*****************************************
+     * 計算上の取得年月
+     *****************************************/
+    Date acquisitionDateNominal(){
+      return Date(_acquisitionDate.year, _acquisitionDate.month, 1);
+    }
+
+    /*****************************************
+     * 計算上の償却期間終了年月
+     *****************************************/
+    Date endDateNominal(){
+      import std.datetime: dur;
+      Date st= acquisitionDateNominal;
+      return Date(st.year+economicLife, st.month, 1) -dur!"days"(1);
+    }
+
+    /*****************************************
+     * 最初の年の有効残月数
+     *****************************************/
+    ubyte remainMonthOfFirstYear() nothrow @nogc{
+      uint result= 13-_acquisitionDate.month;
+      return cast(typeof(return))result;
+    }
+
+    /*****************************************
+     * 償却期間内か否か
+     *****************************************/
+    bool isInDeprecPeriod(in uint year, in Month month= Month.dec){
+      const Date theDay= Date(year, month, daysOfMonth(year, month));
+      return (theDay >= acquisitionDateNominal &&
+	      theDay <= endDateNominal)? true: false;
+    }
+
+    bool isInDeprecPeriod(in Date theDay){
+      return isInDeprecPeriod(theDay.year, theDay.month);
+    }
+
+    /*****************************************
+     * 資産額
+     *****************************************/
+    uint assetValue(in uint year, in Month month= Month.dec){
+      import std.math: round;
+      const Date theDay= Date(year, month, daysOfMonth(year, month));
+      double result;
+
+      if(theDay < acquisitionDateNominal){	// 取得前
+	result= 0;
+      }
+      else{	// 取得後
+	/*
+	 * acquisition: 2010-02
+	 * calc: 2012-11
+	 * duration= -1+12*2+11= 34
+	 *
+	 * 2012-2010+ 11-2+1
+	 */
+	if(isInDeprecPeriod(year, month)){
+	  const uint durInMonth= theDay.year-acquisitionDateNominal.year
+	    +theDay.month-acquisitionDateNominal.month+1;
+	  double price= _priceInit-cast(double)priceDeprecBasic/12*durInMonth;
+	  price= round(price);
+	}
+	else{
+	  result= 1;	// 備忘価格
+	}
+      }
+      return cast(typeof(return))result;
+    }
+
+    /*****************************************
+     * 償却金額
+     *****************************************/
+    uint priceDeprec(string Period)(in uint year, in Month month= Month.dec)
+    if(Period == "year" || Period == "month"){
+      typeof(return) result;
+
+      if(isInDeprecPeriod(year, month)){
+	static if(Period == "year"){
+	  if(year == _acquisitionDate.year) result= priceDeprecFirst;
+	  else result= assetValue(year-1)-assetValue(year);
+	}
+	else{
+	  import std.math: round;
+	  return cast(typeof(return))round(priceDeprecBasic/12.0);
+	}
+      }
+      else{
+	result= 0;
+      }
+      return result;
+    }
+  }
+
+private:
+  Date _acquisitionDate;
+  uint _priceInit;
+  uint _economicLife;
+  string _accountTitle;
+
+  @safe pure nothrow @nogc{
+    uint priceDeprecFirst() const{
+      import std.math: round;
+      const double validRate= cast(double)(remainMonthOfFirstYear)/12.0;
+      return cast(typeof(return))round(validRate*this.priceDeprecBasic);
+    }
+
+    uint daysOfMonth(in uint year, in Month month) const{
+      typeof(return) result;
+      final switch(month){
+      case Month.jan, Month.mar, Month.may, Month.jul, Month.aug, Month.oct, Month.dec:
+	result= 31;
+	break;
+      case Month.apr, Month.jun, Month.sep, Month.nov:
+	result= 30;
+	break;
+      case Month.feb:
+	result= (year%400 == 0 || (year%4 == 0 && year%100 != 0))? 29: 28;
+      }
+      return result;
+    }
+  }
+}
---
+++ titles.d
@@ -0,0 +1,148 @@
+module bookkeeping.titles;
+
+/*************************************************************
+ * 勘定科目の区分
+ *************************************************************/
+enum AccountCategory{asset,	// 資産
+		     liability,	// 負債
+		     equity,	// 資本
+		     revenue,	// 収益
+		     expense}	// 費用
+
+/*************************************************************
+ * 勘定科目の一覧
+ *************************************************************/
+immutable string[154] accountTitles;
+immutable string[5] inventoryTitles;
+
+/*************************************************************
+ * 仕訳データ
+ *************************************************************/
+struct AccountValue{
+  this(in string title) @safe pure nothrow @nogc{
+    import std.algorithm: countUntil;
+    switch(accountTitles[].countUntil(title)){
+    case 0: .. case 57:
+      _category= AccountCategory.asset;
+      break;
+    case 58: .. case 69:
+      _category= AccountCategory.equity;
+      break;
+    case 70: .. case 72:
+      _category= AccountCategory.liability;
+      break;
+    case 73: .. case 93:
+      _category= AccountCategory.revenue;
+      break;
+    case 94: .. case 155:
+      _category= AccountCategory.expense;
+      break;
+    default:
+      assert(false);
+    }
+
+    _title= title;
+    priceDebit= 0;
+    priceCredit= 0;
+  }
+
+  int balanceLast;
+  int priceDebit;
+  int priceCredit;
+
+  @safe pure const{
+    /*****************************************
+     * 勘定科目
+     *****************************************/
+    string title() nothrow @nogc{
+      return _title;
+    }
+
+    /*****************************************
+     * 勘定科目の区分
+     *****************************************/
+    AccountCategory category() nothrow @nogc{
+      return _category;
+    }
+
+    /*****************************************
+     * 期間内の決算
+     *****************************************/
+    int balanceThisTerm() nothrow @nogc{
+      typeof(return) result= 0;
+      final switch(_category){
+      case AccountCategory.asset, AccountCategory.expense:
+	result= priceDebit-priceCredit;
+	break;
+      case AccountCategory.liability, AccountCategory.equity, AccountCategory.revenue:
+	result= -priceDebit+priceCredit;
+      }
+
+      return result;
+    }
+
+    /*****************************************
+     * 累計の決算
+     *****************************************/
+    int balanceTotal() nothrow @nogc{
+      return balanceThisTerm +balanceLast;
+    }
+  }
+
+private:
+  string _title;
+  AccountCategory _category;
+}
+
+shared static this(){
+  accountTitles[]= () @safe pure nothrow @nogc{
+    const string[154] dummy= [
+      0: "現金", "当座預金", "普通預金（JAバンク）", "定期預金", // 資産 [0..58]
+      "定期積立", "その他預金", "受取手形", "売掛金",
+      "貸倒引当金", "有価証券", "製品", "原材料",
+      "仕掛品", "貯蔵品", "前渡金", "前払費用",
+      "未収消費税等", "短期貸付金", "未収入金", "預け金",
+      "立替金", "仮払金", "建物", "建物付属設備",
+      "構築物", "機械装置", "車輌運搬具", "器具備品",
+      "生物", "繰延生物", "一括償却資産", "土地",
+      "建設仮勘定", "育成仮勘定", "商標権", "実用新案権",
+      "意匠権", "育成者権", "ソフトウェア", "土地改良負担金",
+      "借家権", "借地権", "電話加入権", "投資有価証券",
+      "関係会社株式", "出資金", "関係会社出資金", "長期貸付金",
+      "破産等債権", "長期前払費用", "客土", "保険積立金",
+      "経営保険積立金", "長期預け金", "開業費", "開発費",
+      "事業主貸", "未収穫農産物",
+      58: "買掛金", "短期借入金", "未払金", "未払費用",	// 負債 [58..69]
+      "未払消費税等", "前受金", "預り金", "仮受金",
+      "長期借入金", "長期未払金", "農業経営基盤強化準備金", "事業主借",
+      70: "元入金", "資本金", "株主資本",	// 資本 [70..72]
+      73: "製品売上高", "生物売却収入", "作業受託収入", "価格補填収入",	// 収益[73..93]
+      "生物売却原価", "事業消費高", "受取利息", "受取配当金",
+      "受取地代家賃", "受取共済金", "一般助成収入", "作付助成収入",
+      "飼糧補填収入", "雑収入", "固定資産売却益", "投資有価証券売却益",
+      "経営安定補填収入", "収入保険補填収入", "保険差益", "償却債権取立益",
+      "貸倒引当金戻入額",
+      "期首商品製品棚卸高", "期末商品製品棚卸高", 
+      94: "荷造運賃", "販売手数料", "広告宣伝費", "交際費",	// 費用[94..156]
+      "旅費交通費", "事務通信費", "車輌費", "図書研修費",
+      "支払報酬", "減価償却費", "開発費償却", "地代家賃",
+      "支払保険料", "諸会費", "寄附金", "貸倒引当金繰入額",
+      "雑費", "種苗費", "素畜費", "肥糧費",
+      "飼糧費", "農薬費", "敷料費", "燃油費",
+      "諸材料費", "賃金手当", "作業用衣料費", "作業委託費",
+      "診療衛生費", "預託費", "ヘルパー利用費", "圃場管理費",
+      "農具費", "修繕費", "動力光熱費", "共済掛金",
+      "とも補償拠出金", "農地賃借料", "地代賃借料", "土地改良費",
+      "租税公課（一般管理費分）", "租税公課（製造経費分）", "支払利息", "手形譲渡損",
+      "廃畜処分損", "雑損失", "固定資産売却損", "固定資産除却損",
+      "固定資産圧縮損", "災害損失", "特別償却費", "給料手当",
+      "雑給", "賞与", "退職金", "退職給付費用",
+      "法定福利費", "福利厚生費",
+      "期首材料棚卸高", "期末材料棚卸高"];
+    return dummy;
+  }();
+
+  inventoryTitles= ["未収穫農産物",
+		    "期首商品製品棚卸高", "期末商品製品棚卸高",
+		    "期首材料棚卸高", "期末材料棚卸高"];
+}
